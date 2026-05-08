34b5ad2df9eacfa7ab0f885c781aee6bb5d9ffb9
Lucy Dryaeva <48590492+ShadiestGoat@users.noreply.github.com>
2026-05-08 18:46:12 +0100

    fixup commands

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"net/http"
+	"log/slog"
+	"net"
 	"os"
 	"os/signal"
 
 	"github.com/shadiestgoat/bankDataDB/config"
-	"github.com/shadiestgoat/bankDataDB/db"
-	"github.com/shadiestgoat/bankDataDB/db/store"
-	"github.com/shadiestgoat/bankDataDB/external"
-	"github.com/shadiestgoat/bankDataDB/internal"
-	"github.com/shadiestgoat/bankDataDB/log"
+	"github.com/shadiestgoat/bankDataDB/grpc/bank_data"
+	"github.com/shadiestgoat/bankDataDB/grpc/user_svc"
+	"github.com/shadiestgoat/bankDataDB/pb/bank_svc_pb"
+	"github.com/shadiestgoat/bankDataDB/pb/user_svc_pb"
+	"google.golang.org/grpc"
 
 	_ "github.com/shadiestgoat/bankDataDB/bank_parser/all"
 )
+
+func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+	}))
+	slog.SetDefault(logger)
+}
 
 func main() {
 	cleanup := config.LoadBasics()
@@ -26,36 +33,34 @@ func main() {
 		port = "3000"
 	}
 
-	apiDB := db.GetDB(log.NewCtxLogger().With("parent_module", "internal"))
-	a := internal.NewAPI("http_server", log.NewCtxLogger(), &internal.APIConfig{
-		JWT: &internal.JWTConfig{Secret: []byte(config.JWT_SECRET)},
-	}, apiDB, store.NewStore(apiDB))
+	grpcSRV := grpc.NewServer(
+		grpc.UnaryInterceptor(bank_data.NewAuthInterceptor()),
+	)
+	bank_svc_pb.RegisterBankDataServer(grpcSRV, bank_data.NewAPI())
+	user_svc_pb.RegisterUserServiceServer(grpcSRV, user_svc.NewAPI())
 
-	r := external.Router(a, store.NewStore(db.GetDB(log.NewCtxLogger().With("parent_module", "external"))))
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+	
 
-	a.Logger()(context.Background()).Infow("Loading server", "port", port)
-
-	s := &http.Server{Addr: ":" + port, Handler: r}
-	closingServer := make(chan bool)
-
-	go func() {
-		err := s.ListenAndServe()
-		if !errors.Is(err, http.ErrServerClosed) {
-			a.Logger()(context.Background()).Errorw("Closing HTTP Server", "error", err)
-		} else {
-			a.Logger()(context.Background()).Debugw("Server has closed intentionally(?)")
-		}
-
-		close(closingServer)
-	}()
-
+	grpcEnded := make(chan bool)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
+	go func() {
+		err = grpcSRV.Serve(lis)
+		if err != nil {
+			slog.Error("GRPC Ended with error", "error", err)
+			close(grpcEnded)
+		}
+	}()
+
 	select {
 	case <-c:
-		s.Close()
-	case <-closingServer:
+		grpcSRV.GracefulStop()
+	case <-grpcEnded:
 		panic("Existing server (not happy)...")
 	}
 }
